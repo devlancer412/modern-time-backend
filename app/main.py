1bc6b7fcc8d8dc7f526be4de2317090b26b4aeaf
DevLancer <DevAnderson0412@gmail.com>
2022-06-28 11:09:45 -0400

    cors setting

--- app/main.py
+++ app/main.py
@@ -11,11 +11,9 @@ Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
-origins = ["http://localhost:8000"]
-
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins="*",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
