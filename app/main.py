954c709fa8124757ffdcb106d85c2d6f93c2b79f
DevLancer <DevAnderson0412@gmail.com>
2022-06-29 23:02:34 -0400

    minimize

--- app/main.py
+++ app/main.py
@@ -21,10 +21,6 @@ app = FastAPI(
     version="-".join([__version__, __status__]),
 )
 
-if __name__ == '__main__':
-    uvicorn.run("main:app", host="0.0.0.0", port=80, log_level="info", reload = True)
-    print("running")
-    
 bootstrap(app)
 
 database = Database()
