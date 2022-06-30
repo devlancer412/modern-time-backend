0e534d23fd9534e03348c2be57fd85db395adbcf
DevLancer <DevAnderson0412@gmail.com>
2022-06-30 10:45:06 -0400

    api router added

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,10 @@
 from fastapi import APIRouter
 from controllers import auth
 
-router = APIRouter()
+router = APIRouter(
+  prefix='/api',
+  tags=['api'],
+  responses={404: {"description": "Not found"}},
+)
+
 router.include_router(auth.router)
